ea83bcf762bd9cefdbc97e296b0f4ae23b880885
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2023-01-05 12:29:09 +0100

    update: move the Home page into components

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home(props) {
+  return (
+    <div className="home">
+      <h2 id="home_title">Quizzical</h2>
+      <p id="home_paragraph">Play the quiz and see how well you perform!</p>
+      <button id="home_button" onClick={props.handleClick}>Start quiz</button>
+    </div>
+  )
+}
