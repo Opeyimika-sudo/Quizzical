3fc24ac01bc1b63bb1d8d56e9386e46c0bb193e2
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2023-01-05 18:00:38 +0100

    feat: add eventListener to the 'start quiz' button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ export default function Home(props) {
     <div className="home">
       <h2 id="home_title">Quizzical</h2>
       <p id="home_paragraph">Play the quiz and see how well you perform!</p>
-      <button id="home_button" onClick={props.handleClick}>Start quiz</button>
+      <button id="home_button" onClick={() => props.handleClick()}>Start quiz</button>
     </div>
   )
 }
