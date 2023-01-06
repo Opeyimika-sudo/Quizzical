55d32817db64812cc32e7b072d0132ea472f3337
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2023-01-06 01:48:24 +0100

    feat: set the preliminary logic to change state for checked options

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,12 +3,15 @@ import {decode} from 'html-entities';
 
 export default function Question(props) {
     let choices = props.data.multichoice;
+    console.log(choices);
   return (
     <section>
             <h2 className="quiz_question">{decode(props.data.question)}</h2>
             <ul className="quiz_options">
                 {choices.map((item, index) => (
-                    <li className="quiz_option" key={index}>{decode(item)}</li>
+                    <li className="quiz_option"
+                     onClick={()=>props.handleClick(props.id, index)}
+                    key={index}>{decode(item.string)}</li>
                 ))}
             </ul>
             <hr />
