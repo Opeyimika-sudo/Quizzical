ab2eea3df1274b613c3df676d19f47caac7bf8e1
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2023-01-06 23:38:25 +0100

    feat: add inline styling for clear differentiation of right and wrong answers with ternary conditionals

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,14 +3,35 @@ import {decode} from 'html-entities';
 
 export default function Question(props) {
     let choices = props.data.multichoice;
+    
   return (
     <section>
             <h2 className="quiz_question">{decode(props.data.question)}</h2>
             <ul className="quiz_options">
                 {choices.map((item, index) => (
                     <li className="quiz_option" style={{
-                        backgroundColor: choices[index].isChecked ? '#d6dbf5' : '#f5f7fb',
-                        border: choices[index].isChecked ? 'none' : '0.794239px solid #4D5B9E'
+                        backgroundColor: props.isSubmitted ? 
+                            item.isCorrect
+                            ? 
+                            '#94d7a2' 
+                            : 
+                            item.isChecked ? '#F8BCBC' :'#f5f7fb' 
+                         : item.isChecked ? '#d6dbf5' : '#f5f7fb',
+                        border: props.isSubmitted ? 
+                                item.isCorrect
+                                ? 
+                                'none'
+                                : 
+                                item.isChecked ? 'none' :'0.771045px solid #4D5B9E' 
+                        : item.isChecked ? 'none' : '0.794239px solid #4D5B9E',
+                        opacity: props.isSubmitted ?
+                                item.isCorrect
+                                ? 
+                                '1'
+                                : 
+                                item.isChecked ? '1' :'0.5' 
+                                : '1', 
+                        
                     }}
                      onClick={()=>props.handleClick(props.id, index)}
                     key={index}>{decode(item.string)}</li>
