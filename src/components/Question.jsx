4760ccb1fab74dbdca4e7bf06a5c635e52dfeb49
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2023-01-06 13:44:49 +0100

    feat: add inline styling to differentiate option chosen from others

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -8,7 +8,10 @@ export default function Question(props) {
             <h2 className="quiz_question">{decode(props.data.question)}</h2>
             <ul className="quiz_options">
                 {choices.map((item, index) => (
-                    <li className="quiz_option"
+                    <li className="quiz_option" style={{
+                        backgroundColor: choices[index].isChecked ? '#d6dbf5' : '#f5f7fb',
+                        border: choices[index].isChecked ? 'none' : '0.794239px solid #4D5B9E'
+                    }}
                      onClick={()=>props.handleClick(props.id, index)}
                     key={index}>{decode(item.string)}</li>
                 ))}
